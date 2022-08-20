b577b68c21446a4c94580d8ca268928d7da1876b
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-08-21 02:09:58 +0530

    commit

---
+++ AMAZON/stockprice.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long int
+#define pb push_back
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+void solve()
+{
+    int n;  cin >> n;
+    int a[n]; fl(i, n) cin >> a[i];
+
+    vector<int> pre(n, 0), suff(n, 0);
+    pre[0] = a[0];
+    suff[n - 1] = a[n - 1];
+    for (int i = 1; i < n; i++)  pre[i] = pre[i - 1] + a[i];
+    for (int i = n - 2; i >= 0; i--)  suff[i] = a[i] + suff[i + 1];
+
+    int ans = INT_MAX, month = 1;
+
+    for (int i = 0; i < n; i++){
+        int x = pre[i] / (i + 1),y;
+        int div = n - i - 1;
+        if(div==0)  div = 1;
+        if (i < n - 1)   y = (suff[i + 1]) / div;
+        else y = (suff[i]-suff[i-1]) / div;
+
+        int temp = abs(x - y);
+        if (temp < ans)  {
+            ans = temp;
+            month = i + 1;
+        }
+    }
+    cout << month << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
