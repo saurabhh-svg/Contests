b77ba570f4f300f786ec33d72715e824b5589865
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-03-18 17:16:50 +0530

    ICPC + COdechef starters + OA

---
+++ E_1_Unforgivable_Curse_easy_version.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    string s, t;
+    cin >> s >> t;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int test;
+    cin >> test;
+    while (test--)
+        solve();
+}
